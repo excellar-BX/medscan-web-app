8c1576ba383051f6a51f88970d38ce30237ecae5
kingxalpha <tosinsirmuel@gmail.com>
2024-09-05 18:58:49 +0100

    updates on login

--- src/components/AddNewPro.jsx
+++ src/components/AddNewPro.jsx
@@ -79,9 +79,9 @@ export default function AddNewPro() {
       };
   
       console.log("productData:", productData);
-  
+      // http://localhost:5000/api/products/create
       try {
-        const response = await fetch("http://localhost:5000/api/products/create", {
+        const response = await fetch("https://medscan-backend.vercel.app/api/products/create", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
