801f3b7b8bf9239d369835c19ad4eb3d2b740a15
unknown <dhawau29@gmail.com>
2024-09-10 15:58:45 +0100

    changes in the signinDistributor and logDistributor

--- src/components/LogDistributor.jsx
+++ src/components/LogDistributor.jsx
@@ -175,7 +175,8 @@ export default function LogDistributor() {
             <button
               type="submit"
               disabled={isSubmitting}
-              className="w-full bg-red-500 text-white py-2 rounded-lg"
+             className="w-full bg-blue-400 text-white py-4 rounded-lg hover:bg-green-400 transition-all duration-300 ease-in-out"
+
             >
               {isSubmitting ? 'Submitting...' : 'Create Account'}
             </button>
