b00237fd3d1b284240d032d9d38c215a126d32e9
unknown <dhawau29@gmail.com>
2024-09-11 05:55:01 +0100

    changes in the logDistributor and phoneNumber

--- src/components/LogDistributor.jsx
+++ src/components/LogDistributor.jsx
@@ -10,8 +10,13 @@ import bgImage from "../assets/images/login-img.jpg";
 
 const LocalSignupSchema = Yup.object().shape({
   fullName: Yup.string().required('Full Name is required'),
+  businessName: Yup.string().required('business Name is required'),
   email: Yup.string().email('Invalid email').required('Email is required'),
   phone: Yup.string().required('Phone number is required'),
+  businessDateOfEstab: Yup.string().required('business Date is required'),
+  businessLocation: Yup.string().required('business Location is required'),
+  businessRegNumber: Yup.string().required('businessReg Number is required'),
+  taxIdNumber: Yup.string().required('tax Id is required'),
   password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref('password'), null], 'Passwords must match')
@@ -66,8 +71,13 @@ export default function LogDistributor() {
       <Formik
         initialValues={{
           fullName: '',
+          businessName: '',
           email: '',
           phone: '',
+          businessDateOfEstab: '',
+          businessLocation: '',
+          businessRegNumber: '',
+          taxIdNumber: '',
           password: '',
           confirmPassword: '',
           agreeToTerms: false,
@@ -110,10 +120,24 @@ export default function LogDistributor() {
                 className="text-red-500 text-xs"
               />
             </div>
+            <div className="relative border border-gray-300 rounded-lg">
+              <label className="absolute top-0 left-2 bg-white text-gray-500 text-sm px-1 -translate-y-1/2">
+                businessName
+              </label>
+              <Field
+                name="businessName"
+                className="w-full px-4 py-2 bg-transparent border-none outline-none"
+              />
+              <ErrorMessage
+                name="businessName"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
 
             <div className="relative border border-gray-300 rounded-lg">
               <label className="absolute top-0 left-2 bg-white text-gray-500 text-sm px-1 -translate-y-1/2">
-                Email
+                businessEmailAddress
               </label>
               <Field
                 type="email"
@@ -131,6 +155,66 @@ export default function LogDistributor() {
               {({ field, form }) => <PhoneNumberInput field={field} form={form} />}
             </Field>
             <ErrorMessage name="phone" component="div" className="text-red-500 text-xs" />
+            <div className="relative border border-gray-300 rounded-lg">
+              <label className="absolute top-0 left-2 bg-white text-gray-500 text-sm px-1 -translate-y-1/2">
+              businessDateOfEstab
+              </label>
+              <Field
+              type="date"
+                name="businessDateOfEstab"
+                className="w-full px-4 py-2 bg-transparent border-none outline-none"
+              />
+              <ErrorMessage
+                name="businessDateOfEstab"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
+            <div className="relative border border-gray-300 rounded-lg">
+              <label className="absolute top-0 left-2 bg-white text-gray-500 text-sm px-1 -translate-y-1/2">
+                businessLocation
+              </label>
+              <Field
+                name="businessLocation"
+                className="w-full px-4 py-2 bg-transparent border-none outline-none"
+              />
+              <ErrorMessage
+                name="businessLocation"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
+            <div className="relative border border-gray-300 rounded-lg">
+              <label className="absolute top-0 left-2 bg-white text-gray-500 text-sm px-1 -translate-y-1/2">
+                businessRegNumber
+              </label>
+              <Field
+              type="text"
+                name="businessRegNumber"
+                className="w-full px-4 py-2 bg-transparent border-none outline-none"
+              />
+              <ErrorMessage
+                name="businessRegNumber"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
+      
+            <div className="relative border border-gray-300 rounded-lg">
+              <label className="absolute top-0 left-2 bg-white text-gray-500 text-sm px-1 -translate-y-1/2">
+                taxIdNumber
+              </label>
+              <Field
+              type="text"
+                name="taxIdNumber"
+                className="w-full px-4 py-2 bg-transparent border-none outline-none"
+              />
+              <ErrorMessage
+                name="taxIdNumber"
+                component="div"
+                className="text-red-500 text-xs"
+              />
+            </div>
 
             <div className="relative border border-gray-300 rounded-lg">
               <label className="absolute top-0 left-2 bg-white text-gray-500 text-sm px-1 -translate-y-1/2">
